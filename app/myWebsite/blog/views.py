d0e6893a903b2abd5c161e53b9f7dfd9acd41314
Ali Zulfikar <alizulfikar032@gmail.com>
2022-06-06 21:50:55 +0700

    Learn: App

--- app/myWebsite/blog/views.py
+++ app/myWebsite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def blog(request):
     return render(request, 'blog.html')
+
+def recent(response):
+    return HttpResponse('Halo ini responses dari Recent!')
